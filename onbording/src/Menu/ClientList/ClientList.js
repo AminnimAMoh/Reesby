56aac2bc2fab0d5ca3d7433990f15414f74e703e
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-03-04 11:42:10 +1100

    Version .02 Responsive-StyleOne
    -Changed the styling system to MaterialUI
    -Add Full responsiveness to all grids

--- onbording/src/Menu/ClientList/ClientList.js
+++ onbording/src/Menu/ClientList/ClientList.js
@@ -1,47 +1,105 @@
 import React from 'react'
-import "./ClientList.css";
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import EditIcon from '@material-ui/icons/Edit';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    List__Grid: {
+        borderBottom: "1px solid rgb(230, 230, 230)",
+        padding: theme.spacing(1),
+    },
+    heighWeightTypo: {
+        fontWeight: 700,
+    },
+    List_button: {
+        width: "50%",
+        borderRadius: "12px",
+        border: "2px double gray",
+        textAlign: "center",
+        [theme.breakpoints.down('sm')]: {
+            height: "35px",
+            width: "25%",
+            borderRadius: "6px",
+            border: "none"
+        }
+    },
+    List_Checkbox: {
+        marginLeft: theme.spacing(-2),
+        marginRight: theme.spacing(2),
+    },
+    List_Headings: {
+        fontWeight: 700,
+        [theme.breakpoints.up('md')]: {
+            display: "none",
+        }
+    },
+    List_Blank:{
+        fontStyle: "italic",
+        color: "rgb(211, 211, 211)",
+    }
+}));
 
 function ClientList(props) {
+    const classes = useStyles();
     // This page recives the data from Menu.js component and renders its information in a grid
     return (
-        <div>
-            <div className="Menu__Listgrid">
-                <Grid container spacing={1} justify="center" alignItems="center" className="Menu_GridList">
-                    <Grid container item xs={1}>
-                        <input type="checkbox"></input>
-                    </Grid>
-                    <Grid container item xs={1}>
-                        <p style={{ fontWeight: "700" }}>{props.data.clientName}</p>
-                    </Grid>
-                    <Grid container item xs={3}>
-                        <p>{props.data.clientEmail}</p>
-                    </Grid>
-                    <Grid container item xs={1}>
-                        <p>{props.data.clientWorkPhone}</p>
-                    </Grid>
-                    <Grid container item xs={1}>
-                        <p>{props.data.clientIndustry}</p>
-                    </Grid>
-                    <Grid container item xs={1}>
-                        <p>{props.data.clientIndustry}</p>
-                    </Grid>
-                    <Grid container item xs={3}>
-                        <p>{props.data.clientWebsite}</p>
-                    </Grid>
-                    <Grid container item xs={1}>
-                        <div style={{ borderRadius: "12px", border: "1px solid gray" }}>
-                            <IconButton aria-label="delete" disabled color="primary">
-                                <EditIcon />
-                            </IconButton>
-                        </div>
-                    </Grid>
-
+        <Grid container spacing={1} justify="center" alignItems="center" className={classes.List__Grid}>
+            <Grid container spacing={1} xs={1} justify="center" alignItems="center" className={classes.List_Checkbox}>
+                <Grid item xs={12} style={{ textAlign: "center" }}>
+                    <input type="checkbox"></input>
                 </Grid>
-            </div>
-        </div>
+            </Grid>
+            <Grid container spacing={1} xs={4} justify="center" alignItems="center" className={classes.List_Headings}>
+                <Grid item xs={12} className={classes.heighWeightTypo}>
+                    <p style={{ fontWeight: "700" }}>Client Name</p>
+                </Grid>
+                <Grid item xs={12}>
+                    <p>Email</p>
+                </Grid>
+                <Grid item xs={12}>
+                    <p>Phone</p>
+                </Grid>
+                <Grid item xs={12}>
+                    <p>Industry</p>
+                </Grid>
+                <Grid item xs={12}>
+                    <p>Point of Contact</p>
+                </Grid>
+                <Grid item xs={12}>
+                    <p>Website</p>
+                </Grid>
+            </Grid>
+            <Grid container spacing={1} xs={5} md={10} justify="center" alignItems="center">
+                <Grid item xs={12} md={1} className={classes.heighWeightTypo}>
+                        {(props.data.clientName) ? <p style={{ fontWeight: "700" }}>{props.data.clientName}</p> : <p className={classes.List_Blank}>Blank</p>}
+                </Grid>
+                <Grid item xs={12} md={3}>
+                    {(props.data.clientEmail) ? <p>{props.data.clientEmail}</p> : <p className={classes.List_Blank}>Blank</p>}
+                </Grid>
+                <Grid item xs={12} md={1}>
+                {(props.data.clientWorkPhone) ? <p>{props.data.clientWorkPhone}</p> : <p className={classes.List_Blank}>Blank</p>}
+                </Grid>
+                <Grid item xs={12} md={2}>
+                {(props.data.clientIndustry) ? <p>{props.data.clientIndustry}</p> : <p className={classes.List_Blank}>Blank</p>}
+                </Grid>
+                <Grid item xs={12} md={2}>
+                {(props.data.clientPocName) ? <p>{props.data.clientPocName}</p> : <p className={classes.List_Blank}>Blank</p>}
+                </Grid>
+                <Grid item xs={12} md={3}>
+                {(props.data.clientWebsite) ? <p>{props.data.clientWebsite}</p> : <p className={classes.List_Blank}>Blank</p>}
+                </Grid>
+            </Grid>
+            <Grid container spacing={1} xs={2} md={1} justify="center" alignItems="center">
+                <Grid item xs={12}>
+                    <div className={classes.List_button}>
+                        <IconButton size="small" aria-label="delete" disabled color="primary">
+                            <EditIcon style={{ color: "#5111a2" }} />
+                        </IconButton>
+                    </div>
+                </Grid>
+            </Grid>
+        </Grid >
     )
 }
 
--- onbording/src/Menu/Menu.js
+++ onbording/src/Menu/Menu.js
@@ -1,9 +1,10 @@
 import { React, useEffect, useState } from 'react';
 //Importing the Menu page Style sheet 
-import "./Menu.css";
+// import "./Menu.css";
 //Axios library to read the api and sort it
 import axios from "axios";
 
+import { makeStyles } from '@material-ui/core/styles';
 //Importing ClientList.js that renders the data information
 import ClientList from "./ClientList/ClientList";
 
@@ -17,8 +18,107 @@ import AddIcon from '@material-ui/icons/Add';
 import EditIcon from '@material-ui/icons/Edit';
 import IconButton from '@material-ui/core/IconButton';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        padding: "25px",
+    },
+    heighWeightTypo: {
+        fontWeight: 700,
+        backgroundColor: "rgb(214, 214, 214)",
+        fontStyle: "italic",
+        padding: theme.spacing(1),
+        [theme.breakpoints.down('sm')]:{
+            display: "none",
+        }
+    },
+    Menu__HeadingFlex: {
+        // Need Fix
+        position: "relative",
+        width: "40%",
+        marginTop: "25px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    Menu__search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: "white",
+        '&:hover': {
+            backgroundColor: "gray",
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    Menu__searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+                width: '20ch',
+            },
+        },
+    },
+    Menu__button: {
+        textAlign: "center",
+        margin: theme.spacing(1),
+        [theme.breakpoints.down("sm")]: {
+            minWidth: 32,
+            paddingLeft: 8,
+            paddingRight: 8,
+            "& .MuiButton-startIcon": {
+                margin: 0
+            }
+        },
+        textTransform: "capitalize",
+    },
+    Menu__buttonText: {
+        [theme.breakpoints.down("sm")]: {
+            display: "none"
+        }
+    },
+    Menu__List: {
+        borderRadius: "12px",
+        marginTop: "24px",
+        backgroundColor: "white",
+        padding: "4px"
+    },
+    List_button: {
+        borderRadius: "12px", border: "1px double gray",
+        textAlign: "center"
+    },
+    Menu_Search:{
+        height: "50px",
+        [theme.breakpoints.up("md")]: {
+            marginTop: theme.spacing(5)
+        }
+    }
+}));
+
 
 function Menu() {
+    const classes = useStyles();
     //Reserving API url in a variable
     const rource = "http://javareesbyapi-env.eba-rtdeyeqd.ap-southeast-2.elasticbeanstalk.com/api/v1/getallclients/tenant/reesby";
     //Using a constat for reserving the data from API
@@ -33,99 +133,104 @@ function Menu() {
     }, [])
 
     return (
-        <div className="Menu">
+        <div className={classes.root}>
             {/*Heading names */}
-            <p>Managment</p>
-            <h1>Clients</h1>
-
-            {/*A container holding heading objects */}
-            <div className="Menu_heading">
-                {/* Using a grid for menu to split the width in to */}
-                <Grid container spacing={1}>
-                    {/* Larg width grid */}
-                    {/* To push the Search and filter buttons to the right side */}
-                    <Grid container item xs={11}>
-                        {/* Flex container for Search bar and buttons */}
-                        <div className="Menu__HeadingFlex">
-                            {/* Serach bar container */}
-                            <div className="Menu__SearchBar">
-                                <div className="Menu__SearchIcon">
-                                    {/* Search Icon */}
-                                    <SearchIcon />
-                                </div>
-
-                                {/* Search bar input */}
-                                <InputBase
-                                    placeholder="Search…"
-                                    inputProps={{ 'aria-label': 'search' }}
-                                />
+            <Grid container spacing={1}>
+                <Grid item xs={12}>
+                    <p>Managment</p>
+                </Grid>
+                <Grid item xs={12}>
+                    <h1>Clients</h1>
+                </Grid>
+                {/* Larg width grid */}
+                {/* To push the Search and filter buttons to the right side */}
+                <Grid container spacing={2} xs={10} className={classes.Menu_Search}>
+                    {/* Flex container for Search bar and buttons */}
+                    <Grid item xs={10} lg={2}>
+                        {/* Serach bar container */}
+                        <div className={classes.Menu__search}>
+                            <div className={classes.Menu__searchIcon}>
+                                <SearchIcon />
                             </div>
-                            {/* buttons */}
-                            <Button variant="contained" style={{ backgroundColor: "white" }}>Search</Button>
-                            <Button variant="contained" style={{ backgroundColor: "white" }}>Clear</Button>
+                            <InputBase
+                                placeholder="Search…"
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }}
+                                inputProps={{ 'aria-label': 'search' }}
+                            />
                         </div>
                     </Grid>
-
-                    {/* Small grid size for right hand side buttons */}
-                    <Grid container item xs={1} direction="column">
-                        <div className="Menu__LeftOptions">
-                            {/* In line styling to make minner changes */}
-                            <Button variant="contained" startIcon={<FilterListIcon />} style={{ backgroundColor: "#5111a2", width: "125px", color: "white" }}>Search</Button>
-                            <br></br>
-                            <Button variant="contained" startIcon={<AddIcon />} style={{ backgroundColor: "white", width: "125px" }}>Clear</Button>
-                        </div>
+                    {/* buttons */}
+                    <Grid item xs={6} lg={1}>
+                        <Button variant="contained" style={{ backgroundColor: "white" }}>Search</Button>
+                    </Grid>
+                    <Grid item xs={6} lg={1}>
+                        <Button variant="contained" style={{ backgroundColor: "white" }}>Clear</Button>
                     </Grid>
                 </Grid>
-            </div>
-            <br></br>
-            {/* Heading of the list */}
-            <div className="Menu__ClientList">
-                <div className="Menu__Listgrid" style={{ padding: "25px" }}>
-                    <h1>All Clients</h1>
-                </div>
-                {/* Main container for list for an esier styling */}
-                <div className="Menu__Listgrid" style={{ backgroundColor: "#f8f8fa" }}>
-                    {/* Sindle manual grid for topick headings of the list */}
-                    <Grid container spacing={1} justify="center" alignItems="center" className="Menu_GridList">
-                        <Grid container item xs={1}>
-                            <input type="checkbox"></input>
-                        </Grid>
-                        <Grid container item xs={1}>
-                            <h3>Client Name</h3>
-                        </Grid>
-                        <Grid container item xs={3}>
-                            <h3>Email</h3>
-                        </Grid>
-                        <Grid container item xs={1}>
-                            <h3>Phone</h3>
-                        </Grid>
-                        <Grid container item xs={1}>
-                            <h3>Industry</h3>
-                        </Grid>
-                        <Grid container item xs={1}>
-                            <h3>Point of Contact</h3>
-                        </Grid>
-                        <Grid container item xs={3}>
-                            <h3>Website</h3>
-                        </Grid>
-                        <Grid container item xs={1}>
-                            <div style={{ borderRadius: "12px", border: "1px solid gray" }}>
-                                <IconButton aria-label="delete" disabled color="primary">
-                                    <EditIcon />
-                                </IconButton>
-                            </div>
-                        </Grid>
 
+                {/* Small grid size for right hand side buttons */}
+                <Grid container spacing={2} xs={2} direction="rows" justify="center">
+                    <Grid item xs={12}>
+                        {/* In line styling to make minner changes */}
+                        <Button variant="contained" startIcon={<FilterListIcon />} className={classes.Menu__Button} style={{ backgroundColor: "#5111a2", color: "white" }}>
+                            <span className={classes.Menu__buttonText}>New Client</span>
+                        </Button>
                     </Grid>
-                </div>
+                    <Grid item xs={12}>
+                        <Button variant="contained" startIcon={<AddIcon />} className={classes.Menu__Button}>
+                            <span className={classes.Menu__buttonText}>Show Filter</span>
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+            {/* Heading of the list */}
+
+            {/* Main container for list for an esier styling */}
+            {/* Sindle manual grid for topick headings of the list */}
+            <div className={classes.Menu__List}>
+                <Grid container xs={12} style={{ padding: "25px" }}>
+                    <h1>All Clients</h1>
+                </Grid>
+                <Grid container spacing={1} justify="center" alignItems="center" className={classes.heighWeightTypo}>
+                    <Grid container spacing={1} xs={1} justify="center" alignItems="center">
+                        <Grid item xs={12} style={{ textAlign: "center" }}>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={1} xs={7} md={10} justify="center" alignItems="center">
+                        <Grid item xs={12} md={1} className={classes.heighWeightTypo}>
+                            <p style={{ fontWeight: "700" }}>Client Name</p>
+                        </Grid>
+                        <Grid item xs={12} md={3}>
+                            <p>Email</p>
+                        </Grid>
+                        <Grid item xs={12} md={1}>
+                            <p>Phone</p>
+                        </Grid>
+                        <Grid item xs={12} md={2}>
+                            <p>Industry</p>
+                        </Grid>
+                        <Grid item xs={12} md={2}>
+                            <p>Point of Contact</p>
+                        </Grid>
+                        <Grid item xs={12} md={3}>
+                            <p>Website</p>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={1} xs={4} md={1} justify="center" alignItems="center">
+                        <Grid item xs={12}>
+                        </Grid>
+                    </Grid>
+                </Grid >
                 {/* Looping through the data and rendering the setup in ClientList.js component */}
                 {
                     ClientsData.map((client) =>
                         <ClientList data={client} />
                     )
                 }
-
-            </div >
+            </div>
         </div >
     )
 }
